9398bd0c05104441a72e8176adac8070e0254af1
Cesar Santana <casc.santana@gmail.com>
2023-08-24 22:52:14 -0600

    feat: implement users directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
+import { useRef } from "react";
+
+import { useCsvToJson } from "./hooks/useCsv";
+import { Table } from "./components/Table";
+
 function App() {
+  const inputFile = useRef<HTMLInputElement>(null);
+
+  const { data, convert, isBusy, isLoaded } = useCsvToJson({
+    Nombre: "name",
+    "Correo Electronico": "email",
+    Telefono: "phone",
+  });
+
+  const openFile = () => {
+    inputFile.current?.click();
+  }
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [file] = event.target.files || [null];
+
+    if (file) {
+      convert(file)
+    }
+  };
+
   return (
-    <>
-      <p>hello world!</p>      
-    </>
+    <div className="p-3 bg-slate-100 h-screen w-full">
+      <div className="container m-auto ">
+        <input type="file" 
+          ref={inputFile} 
+          onChange={handleOnChange} 
+          className="hidden"
+        />
+
+        <div className="text-center py-14">
+          <button onClick={openFile} disabled={isBusy} className="btn btn-primary">
+            {isBusy ? "Loading CSV..." : "Upload CSV"}
+          </button>
+        </div> 
+
+        <Table data={data} isLoading={isBusy} isLoaded={isLoaded} />
+      </div>
+    </div>
   )
 }
 
